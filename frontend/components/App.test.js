b0203921fc632c5d4e857c4437dc2cfa7662d47a
Eliezer Lara <eliezerlara@Eliezers-MacBook-Air.local>
2025-12-05 22:27:11 -0500

    W_S9_CHallenge

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,32 @@
-// Write your tests here
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import AppFunctional from './AppFunctional';
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
-})
+  expect(true).toBe(true)
+});
+
+test('renders without errors', () => {
+  render(<AppFunctional />);
+});
+
+test('renders initial message', () => {
+  render(<AppFunctional />);
+  const message = screen.getByText(/coordinates/i);
+  expect(message).toBeInTheDocument();
+});
+
+test('renders initial steps', () => {
+  render(<AppFunctional />);
+  const steps = screen.getByText(/time/i);
+  expect(steps).toBeInTheDocument();
+});
+
+test('typing in email input updates its value', () => {
+  render(<AppFunctional />);
+  const emailInput = screen.getByPlaceholderText(/type email/i);
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+  expect(emailInput.value).toBe('test@example.com');
+});
